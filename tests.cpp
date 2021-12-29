48d61e237210442a5dd67e8ee9b91cd069c2045b
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2021-12-28 23:20:58 -0500

    testing size, resize, capacity and push_back

---
+++ tests.cpp
@@ -0,0 +1,53 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "MyVector.h"
+
+TEST_CASE("size, resize, capacity, push_back")
+{
+   MyVector<int> v;
+
+   CHECK(v.size() == 0);
+   CHECK(v.capacity() == 0);
+
+   v.push_back(1);
+
+   CHECK(v.size() == 1);
+   CHECK(v.capacity() == 1);
+
+   v.push_back(2);
+
+   CHECK(v.size() == 2);
+   CHECK(v.capacity() == 2);
+
+   v.push_back(3);
+
+   CHECK(v.size() == 3);
+   CHECK(v.capacity() == 4);
+
+   v.push_back(4);
+
+   CHECK(v.size() == 4);
+   CHECK(v.capacity() == 4);
+
+   v.push_back(5);
+
+   CHECK(v.size() == 5);
+   CHECK(v.capacity() == 8);
+
+   for (int i = 6; i <= 8; i++)
+   {
+      v.push_back(i);
+
+      CHECK(v.size() == i);
+      CHECK(v.capacity() == 8);
+   }
+
+   for (int i = 9; i < 16; i++)
+   {
+      v.push_back(i);
+
+      CHECK(v.size() == i);
+      CHECK(v.capacity() == 16);
+   }
+}
